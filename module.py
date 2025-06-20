a7d001828a9fd50f560cfdeeb76a47655bb1bd31
Priyanka <211002112+coder-priyanka01@users.noreply.github.com>
2025-06-20 20:31:41 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,13 @@
+
+import pyjokes
+
+#print("Hello, World!") \\ python mai joh bhi line # sai start hogi voh comment ho jayegi.
+
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+ This is a
+   multi-line
+     comment
+"""
