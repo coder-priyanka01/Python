1f49a494e8273f4550eb57a526b15a3ed0efa1ad
Priyanka <211002112+coder-priyanka01@users.noreply.github.com>
2025-06-20 21:02:36 +0530

    Add files via upload

---
+++ intro_to_string.py
@@ -0,0 +1,8 @@
+name = "priyanka"
+
+nameshort = name[0:3] #start from index 0 all the way till 3 (not including 3)
+print(nameshort) 
+
+#Access the character at index 1 (second character of the string)
+character1 = name[1]
+print(character1)
