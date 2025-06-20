4ed987272f8cdd60fd45c2004ea7b578266d25a8
Priyanka <211002112+coder-priyanka01@users.noreply.github.com>
2025-06-20 20:40:29 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,41 @@
+# Arithmetic operators 
+
+a = 34
+b = 4
+c = a + b
+print(c) 
+
+# Assignment operators
+a = 4-2 #Assign 4-2 in a
+print(a)
+b = 6
+#b += 3 #increment the value of b by 3 and then assign it to b
+b -= 3 #decrement the value of b by 3 and then assign it to b
+print(b) 
+
+# Comparison operators
+
+d = 5<4 #less than
+print(d) #True or False
+
+d = 5!=4 #not equal to
+print(d) #True or False
+
+#logical operators
+
+e = True or False #and operator
+print(e) #True or False
+
+#truth table of or operator
+print("True or False is" , True or False) 
+print("True or true is", True or True)
+print("False or False is", False or False)
+print("false or true is", False or True)
+
+#truth table of and operator
+print("True and False is" , True and False)
+print("True and true is", True and True)
+print("False and False is", False and False)
+print("false and true is", False and True)
+
+print(not (True))
